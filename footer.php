eabfec9dec53a96b6cacb619c8c68338fd30bd1a
John McAlester <johnmcalester@gmail.com>
2015-12-26 21:45:46 -0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package McAlester
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mcalester' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mcalester' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mcalester' ), 'mcalester', '<a href="https://github.com/johnmcalester" rel="designer">John McAlester</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
