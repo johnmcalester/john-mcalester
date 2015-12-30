5892a5b19bc58874d8166593139856bd1d7d4709
John McAlester <johnmcalester@gmail.com>
2015-12-30 09:45:12 -0800

    Update to footer menu

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mcalester' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mcalester' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mcalester' ), 'mcalester', '<a href="https://github.com/johnmcalester" rel="designer">John McAlester</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mcalester' ), 'McAlester', '<a href="https://github.com/johnmcalester" rel="designer">John McAlester</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
