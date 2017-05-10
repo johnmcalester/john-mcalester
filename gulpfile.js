a37807625ae8ccc442f1a4f6e5f1541c1dd70f6b
John McAlester <johnmcalester@gmail.com>
2017-05-10 10:21:26 -0700

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
-// Require the gulp plugin
+// Require the Gulp task runner plugin
 var gulp = require('gulp');
-// Require the gulp-sass plugin
+// Require the Sass pre-processor plugin
 var sass = require('gulp-sass');
-// Require the run-sequence plugin
+// Require the task sequencer plugin
 var runSequence = require('run-sequence');
+// Require the CSS minify-er plugin
+var cleanCSS = require('gulp-clean-css');
+// Require the file renaming plugin
+var rename = require("gulp-rename");
 
 // Process Sass files
 gulp.task('sass', function(){
   return gulp.src('src/sass/**/*.scss') // Get everything in the src/sass directory
     .pipe(sass()) // Using gulp-sass
+    .pipe(cleanCSS())
+    .pipe(rename("main-min.css"))
     .pipe(gulp.dest('css')) // Put processed file in the css directory
 });
 
