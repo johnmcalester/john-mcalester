e17c951f916d683ead59b32bea9b66767f2b6234
John McAlester <johnmcalester@gmail.com>
2017-05-11 15:02:33 -0700

    Add Gulp tasks and reconfigure architecture

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,39 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 // Require the task sequencer plugin
 var runSequence = require('run-sequence');
+// // Require the source mapping plugin
+var sourcemaps = require('gulp-sourcemaps');
 // Require the CSS minify-er plugin
 var cleanCSS = require('gulp-clean-css');
 // Require the file renaming plugin
 var rename = require("gulp-rename");
 
-
 // Process Sass files
 gulp.task('sass', function(){
-  return gulp.src('src/sass/**/*.scss') // Get everything in the src/sass directory
-    .pipe(sass()) // Using gulp-sass
+  // Get everything in the src/sass directory
+  return gulp.src('src/sass/**/*.scss')
+    // Using gulp-sass
+    .pipe(sass())
+    // Initialize the sourcemap plugin
+    .pipe(sourcemaps.init())
+    // minify CSS
     .pipe(cleanCSS())
+    // Append the sourcemap to the bottom of the minified CSS file
+    .pipe(sourcemaps.write())
+    // Rename our dist CSS with -min
     .pipe(rename("main-min.css"))
-    .pipe(gulp.dest('css')) // Put processed file in the css directory
+    // Put processed file in the correct directory
+    .pipe(gulp.dest('./dist/css'))
 });
 
 // Watch files for changes
 gulp.task('watch', function(){
-  // Gulp watch syntax
+  // Watch for changes in the Sass folder
   gulp.watch('src/sass/**/*.scss', ['sass']);
 });
 
 // Add tasks to the deafult Gulp task
 gulp.task('default', function (callback) {
-  runSequence(['sass','watch'],
-    callback
-  );
+  // Do our tasks in the correct order so we don't miss anything
+  runSequence(['sass','watch'],callback);
 });
