25ac6ff6513fd8eae4d3b8f8cdecfd41f292a359
John McAlester <johnmcalester@gmail.com>
2017-05-11 16:02:40 -0700

    Move heading and subheading into main content area

--- header.php
+++ header.php
@@ -29,8 +29,6 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
@@ -40,7 +38,7 @@
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-	<?php 
+	<?php
 	if ( is_front_page() && !is_paged() && is_dynamic_sidebar('abovecontent') ) {
 		dynamic_sidebar('abovecontent');
 	}
