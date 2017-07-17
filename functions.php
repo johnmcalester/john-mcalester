2f75c1705acb01ccb1efb69b1836ad92ac244e1f
John McAlester <john.mcalester@MBP-994.local>
2017-07-17 13:01:05 -0700

    correct css path

--- functions.php
+++ functions.php
@@ -103,7 +103,7 @@ add_action('wp_enqueue_scripts', 'jmc_enqueue_styles_scripts');
 function jmc_enqueue_styles_scripts() {
 	wp_enqueue_style('gfonts', '//fonts.googleapis.com/css?family=Raleway:300,400,700,900');
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/dist/css/main-min.css' );
+	wp_enqueue_style( 'main', get_template_directory_uri() . 'css/main-min.css' );
 
 	// Note jquery listed as dependancy which prompts WP to load it
 	wp_enqueue_script( 'testtheme-navigation', get_template_directory_uri() . '/src/js/navigation-custom.js', array('jquery') );
